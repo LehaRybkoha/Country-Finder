34e1c1c700001f87f91d9552ec35be77c6729f0d
LehaRybkoha <vintuvintu34@gmail.com>
2020-09-29 02:41:03 +0300

    Main functionality is done

--- src/blocks/modules/input/input.js
+++ src/blocks/modules/input/input.js
@@ -1,18 +1,38 @@
 const submitBtn = document.getElementById('submitBtn');
 
 submitBtn.onclick = () => {
-    getDataFromInput();
+    app();
 };
-function getDataFromInput() {
-    let inputData = document.getElementById('searchInput').value.split(',');
-    return setDataFromFunc(inputData.map((item) => item.trim()))
+const app = () => {
+
+    function getDataFromInput() {
+        const resultList = document.getElementById('resultList')
+        let inputData = document.getElementById('searchInput').value.split(',');
+        let formattedData = inputData.map((item) => item.trim())
+        formattedData.forEach((item, i, arr) => {
+            resultList.insertAdjacentHTML('afterbegin', `
+            <li class="result__item">${item}<span></span></li>
+        `);
+            document.querySelector('.result__item span').classList.add(`arr-item-${arr[i]}`)
+            const api = `https://restcountries.eu/rest/v2/name/${arr[i]}`.replace(/%20/g, '')
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { capital } = data[0];
+                    //Set DOM Elements from the API
+                    document.querySelector(`.arr-item-${arr[i]}`).textContent = capital
+                })
+        });
+
+        return formattedData
+    }
+    getDataFromInput()
+
 }
 
-function setDataFromFunc(inputData) {
-    const resultList = document.getElementById('resultList')
-    return inputData.forEach((item, i, arr) => {
-        resultList.insertAdjacentHTML('beforeend', `
-            <li>${item}</li>
-        `);
-    });
-}
+
+
+//https://restcountries.eu/rest/v2/name/{YourNameCountry}?fields=capital
